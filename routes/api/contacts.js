4c4e9fb0c129dc3e281b061f958269a356581331
IlonaFedorenko <86719203+IlonaFedorenko@users.noreply.github.com>
2023-06-16 16:26:33 +0200

    fix

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const ctrl = require("../../controllers/contacts");
+const { validateBody } = require("../../middlewares");
+const schemas = require("../../schemas/contacts.js");
 
 router.get("/", ctrl.listContacts);
 
 router.get("/:id", ctrl.getContactById);
 
-router.post("/", ctrl.addContact);
+router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
 
 router.delete("/:id", ctrl.removeContact);
 
-router.put("/:id", ctrl.updateContact);
+router.put("/:id", validateBody(schemas.addSchema), ctrl.updateContact);
 
 module.exports = router;
