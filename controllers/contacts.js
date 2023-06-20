00f442e9191e6abadef1f5f5e9b625157bcb1389
IlonaFedorenko <86719203+IlonaFedorenko@users.noreply.github.com>
2023-06-20 19:55:22 +0200

    fix

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -21,6 +21,10 @@ const getContactById = async (req, res, next) => {
 
 const addContact = async (req, res, next) => {
   try {
+    if (!req.body || !req.body.name) {
+      res.status(400).json({ message: "missing required name field" });
+      return;
+    }
     const result = await Contact.create(req.body);
     res.status(201).json(result);
   } catch (error) {
