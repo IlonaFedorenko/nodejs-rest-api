4ffd0793850107846f59be826cd37aa56651f3f9
IlonaFedorenko <86719203+IlonaFedorenko@users.noreply.github.com>
2023-06-20 21:06:27 +0200

    fix

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -2,6 +2,7 @@ const { HttpError } = require("../helpers");
 
 const validateBody = (schema) => {
   const func = (req, res, next) => {
+    // req.body.favorite = "false";
     const { error } = schema.validate(req.body);
     if (error) {
       const name = error.name;
